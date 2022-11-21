49ca1000ea76e43856f7283fecc00ca346138f1f
sonia <soniafaye1@gmail.com>
2022-11-21 16:14:54 -0500

    added book model and started seed file - need to sort out problem for seeding tho

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,87 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Book },
+} = require("../server/db");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({ username: "cody", password: "123" }),
+    User.create({ username: "murphy", password: "123" }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  const books = await Promise.all([
+    Book.create({
+      title: "Wuthering Heights",
+      published_date: "1847",
+      description:
+        "Wuthering Heights is an 1847 novel by Emily Brontë, initially published under the pseudonym Ellis Bell. It concerns two families of the landed gentry living on the West Yorkshire moors, the Earnshaws and the Lintons, and their turbulent relationships with Earnshaw's adopted son, Heathcliff. The novel was influenced by Romanticism and Gothic fiction.",
+      contributer: "",
+      author: "Emily Brontë",
+      publisher: " Nelson Doubleday",
+    }),
+    Book.create({
+      title: "Ethan Frome",
+      published_date: "2015",
+      description:
+        "Ethan Frome toils at his New England farm while struggling to maintain a bearable existence with his sickly and judgemental wife, Zeena. He finds himself increasingly drawn to Mattie, Zeena's cousin, who helps around the house. Although she represents the possibility of happiness, they are trapped by their hopeless situation. And when Zeena becomes suspicious and arranges for Mattie to be sent away, events take a tragic turn. This heart-breaking portrait, from the author of The House of Mirth, is an intimate look at choices not made and lives not yet lived. First published in 1911 and adapted for film, Ethan Frome remains for many the most subtle and moving of Wharton's works.",
+      contributer: "Edith Wharton",
+      author: "Edith Wharton",
+      publisher: "Collins Classics",
+    }),
+    Book.create({
+      title: "Importance of Being Earnest",
+      published_date: "1990",
+      description:
+        "Set in England during the late Victorian era, the play's humour derives in part from characters maintaining fictitious identities to escape unwelcome social obligations. It is replete with witty dialogue and satirises some of the foibles and hypocrisy of late Victorian society. It has proved Wilde's most enduringly popular play.",
+      contributer: "Oscar Wilde",
+      author: "Oscar Wilde",
+      publisher: "Dover PUblications",
+    }),
+    Book.create({
+      title: "Anna Karénina",
+      published_date: "1954",
+      description:
+        "Anna Karenina tells of the doomed love affair between the sensuous and rebellious Anna and the dashing officer, Count Vronsky. Tragedy unfolds as Anna rejects her passionless marriage and must endure the hypocrisies of society. Set against a vast and richly textured canvas of nineteenth-century Russia, the novel's seven major characters create a dynamic imbalance, playing out the contrasts of city and country life and all the variations on love and family happiness. While previous versions have softened the robust, and sometimes shocking, quality of Tolstoy's writing, Pevear and Volokhonsky have produced a translation true to his powerful voice. This award-winning team's authoritative edition also includes an illuminating introduction and explanatory notes. Beautiful, vigorous, and eminently readable, this Anna Karenina will be the definitive text for generations to come.",
+      contributer: "Lev Nikolaevič Tolstoy",
+      author: "Lev Nikolaevič Tolstoy",
+      publisher: "Penguin Books",
+    }),
+    Book.create({
+      title: "Cyrano de Bergerac",
+      published_date: "",
+      description:
+        "Cyrano de Bergerac, verse drama in five acts by Edmond Rostand, performed in 1897 and published the following year. It was based only nominally on the 17th-century nobleman of the same name, known for his bold adventures and large nose.Set in 17th-century Paris, the action revolves around the emotional problems of the noble, swashbuckling Cyrano, who, despite his many gifts, feels that no woman can ever love him because he has an enormous nose. Secretly in love with the lovely Roxane, Cyrano agrees to help his inarticulate rival, Christian, win her heart by allowing him to present Cyrano’s love poems, speeches, and letters as his own work. Eventually Christian recognizes that Roxane loves him for Cyrano’s qualities, not his own, and he asks Cyrano to confess his identity to Roxane; Christian then goes off to a battle that proves fatal. Cyrano remains silent about his own part in Roxane’s courtship. As he is dying years later, he visits Roxane and recites one of the love letters. Roxane realizes that it is Cyrano she loves, and he dies content.",
+      contributer: "",
+      author: "Edmond Rostand",
+      publisher: "Penguin Books",
+    }),
+  ]);
+
+  console.log(`seeded ${users.length} users. seeded ${books.length} books`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+    books: {
+      books: books[0],
+      books: books[1],
+      books: books[2],
+      books: books[3],
+      books: books[4],
+    },
+  };
 }
 
 /*
@@ -32,16 +90,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -51,8 +109,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
---
+++ server/db/models/Book.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Book = db.define("book", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  published_date: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  contributer: {
+    type: Sequelize.STRING,
+  },
+  author: {
+    type: Sequelize.STRING,
+  },
+  publisher: {
+    type: Sequelize.STRING,
+  },
+});
+
+module.exports = Book;
